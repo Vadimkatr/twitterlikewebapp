e4105d3475c59e3892f991f456718e3922f4fd2b
Vadimkatr <vadimlisai@gmail.com>
2019-11-27 15:35:34 +0300

    add validation for user struct

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,9 +1,24 @@
 package model
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+
+)
+
 type User struct {
 	Account_id        int    `json:"id"`
 	Email             string `json:"email"`
 	Username          string `json:"username"`
 	Password          string `json:"password,omitempty"`
 	EncryptedPassword string `json:"-"`
+}
+
+// Validate User fields
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required, validation.Length(6, 100)),
+	)
 }
