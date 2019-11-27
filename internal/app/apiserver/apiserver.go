48ebf23c7665b0a5b4042d385d8099111c5e4089
Vadimkatr <vadimlisai@gmail.com>
2019-11-27 09:51:27 +0300

    server init

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,18 @@
+package apiserver
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func Start() error {
+	r := mux.NewRouter()
+	r.HandleFunc("/register", handleUserCreate()).Methods("POST")
+	return http.ListenAndServe(":8080", r)
+}
+
+func handleUserCreate() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	}
+}
