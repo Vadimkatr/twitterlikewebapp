fcce3a5e0bafa727e060cc52868649b6829ca52a
Vadimkatr <vadimlisai@gmail.com>
2019-11-29 22:01:37 +0300

    refact model package

--- internal/app/model/testing.go
+++ internal/app/model/testing.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// TestUser - in order not to create it many times
 func TestUser(t *testing.T) *User {
 	t.Helper()
 
@@ -15,11 +16,12 @@ func TestUser(t *testing.T) *User {
 	}
 }
 
+// TestTweet in order not to create it many times
 func TestTweet(t *testing.T) *Tweet {
 	t.Helper()
 
 	return &Tweet{
-		Message: "Test tweet",
+		Message:  "Test tweet",
 		UserId:   0,
 		PostTime: time.Now(),
 	}
